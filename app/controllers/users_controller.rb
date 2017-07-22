c107cfb794a9483c4af9f6bd12e975c8be548981
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-22 14:27:37 -0400

    added rack-flash and began coding post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
+  use Rack::Flash
 
   get '/signup' do
     if logged_in?
@@ -9,6 +12,22 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if User.all.find {|user| user.username == params[:username]}
+      flash[:username_message] = "Username already taken"
+      redirect '/signup'
+    elsif User.all.find {|user| user.email == params[:email]}
+      flash[:email_message] = "Email already registered"
+      redirect '/signup'
+    end
+    @user = User.new(params)
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.slug}"
+    else
 
+      redirect
+  end
 
 end
