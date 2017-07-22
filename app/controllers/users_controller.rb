77c1e1366160b11262007980892adec7efa1fab0
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-22 13:35:58 -0400

    signup page in userscontroller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < Application Controller
+
+  get '/signup' do
+    if logged_in?
+      @user = current_user
+      redirect "/users/#{@user.slug}"
+    else
+      erb :'/users/create_user'
+    end
+  end
+end
