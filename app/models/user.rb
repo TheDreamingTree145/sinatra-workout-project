67c166ee046dd61ced669393430aaaedcb17c86c
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-22 14:47:00 -0400

    added slug + self.find_by_slug methods to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates_presence_of :username, :email, :password
+
+  def slug
+    name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|user| user.slug == slug}
+  end
+
 end
