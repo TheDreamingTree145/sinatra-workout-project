84ccbcb6afe5b6c9e403997ff41c90ab80f0642e
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-22 13:26:27 -0400

    added attribute to workouts and ran migration

--- db/migrate/20170722171344_create_workouts.rb
+++ db/migrate/20170722171344_create_workouts.rb
@@ -2,6 +2,7 @@ class CreateWorkouts < ActiveRecord::Migration
   def change
     create_table :workouts do |t|
       t.string :name
+      t.string :created_by
       t.string :category
     end
   end
