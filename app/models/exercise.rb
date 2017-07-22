595a3b879189db084adfb43fcd38d9fa632e361b
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-22 13:10:16 -0400

    coded user class and exercise class

---
+++ app/models/exercise.rb
@@ -0,0 +1,6 @@
+class Exercise < ActiveRecord::Base
+  has_many :user_exercises
+  has_many :users, through: :user_exercises
+  has_many :workout_exercises
+  has_many :workouts, through: :workout_exercises
+end
