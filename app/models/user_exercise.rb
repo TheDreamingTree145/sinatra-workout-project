9692ef1f7dad8882d19b7b5e9bc147a8de76da2d
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-22 13:12:24 -0400

    user_exercise, workout, workout_exercise class coded

---
+++ app/models/user_exercise.rb
@@ -0,0 +1,5 @@
+class UserExercise < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :exercise
+
+end
---
+++ app/models/workout_exercise.rb
@@ -0,0 +1,5 @@
+class WorkoutExercise < ActiveRecord::Base
+  belongs_to :workout
+  belongs_to :exercise
+
+end
