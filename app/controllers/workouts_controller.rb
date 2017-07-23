96bbe95a59f14a57880c56bad550ac7e02fd2a04
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-23 13:49:42 -0400

    fixed creating exercises in workoutcontroller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -32,8 +32,8 @@ class WorkoutsController < ApplicationController
   post '/workouts/new/exercises' do
     @user = current_user
     @all_exercises = Exercise.all
-    if !@all_exercises.empty?
-      if @all_exercises.each {|cise| cise.name.downcase == params[:exercise][:name].downcase}
+    @all_exercises.each do |cise|
+      if cise.name.downcase == params[:exercise][:name].downcase
         flash[:exercise_create_message] = "That exercise already exists"
         redirect '/workouts/new'
       else
@@ -47,16 +47,8 @@ class WorkoutsController < ApplicationController
           redirect '/workouts/new'
         end
       end
-    else
-      @exercise = Exercise.new(params[:exercise])
-      if @exercise.valid?
-        @exercise.save
-        flash[:exercise_create_message] = "Successfully added exercise to database"
-        redirect 'workouts/new'
-      else
-        flash[:exercise_create_message] = "Please make sure all fields are filled out when creating a new exercise"
-        redirect '/workouts/new'
-      end
     end
   end
+
+
 end
