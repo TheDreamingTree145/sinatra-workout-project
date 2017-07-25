a3c0ad410c045d1ba251a5b66605a8feab53a688
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-25 11:39:14 -0400

    finished path for exercise categories show page. add flash to categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,10 +13,14 @@ class CategoriesController < ApplicationController
     end
   end
 
-  get 'categories/exercises/slug' do #restfullness
+  get 'categories/exercises/:name' do #restfullness
     if logged_in?
       @user = current_user
-      erb :'/categories/show_exercises'
+      if @category = WORKOUT_TYPES.find {|cat| cat == params[:name]}
+        erb :'/categories/show_exercises'
+      else
+        flash[:message] = "Category not found!"
+        redirect '/categories'
     else
       flash[:message] = "Please login to view this page"
       redirect '/login'
